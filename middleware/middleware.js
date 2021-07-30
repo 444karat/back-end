e2006083f2e80c55e670258fd40feb8154dd6a41
444karat <sasha7971182@gmail.com>
2021-07-30 10:49:00 +0500

    validate

--- middleware/middleware.js
+++ middleware/middleware.js
@@ -1,4 +1,12 @@
 const validator = require('validator');
+var passwordValidator = require('password-validator');
+
+// Create a schema
+var schema = new passwordValidator();
+schema
+    .is().min(4)
+    .is().max(10)
+    .has().not().spaces();
 
 exports.mdlwr = function (req,res,next){
     const {username , pass, email} = req.body;
@@ -6,8 +14,8 @@ exports.mdlwr = function (req,res,next){
     if (!validator.isEmail(email)){
         return res.json({error: "email is not valid"});
     }
-    /*if (){
-
-    }*/
+    if (!schema.validate(pass)){
+        return res.json({error: "pass is not valid"});
+    }
     next();
 }
